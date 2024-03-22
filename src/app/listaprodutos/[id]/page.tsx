94dd35584025b7b72a33bb10c825a2f1433f86a1
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-22 10:24:39 -0300

    22-03

--- src/app/listaprodutos/[id]/page.tsx
+++ src/app/listaprodutos/[id]/page.tsx
@@ -4,6 +4,8 @@ import produtos from '../../../produtos';
 
 export default function Produto({ params }: { params: { id: string } }) {
 
+    const produto = produtos.filter((prd, index, array) => prd.id === parseInt(params.id));
+
     return (
         <>
             <h1>Produto</h1>
@@ -29,6 +31,19 @@ export default function Produto({ params }: { params: { id: string } }) {
                     }
                 )
             }
+
+            <br />
+
+            <div>
+                <div style={{ display: 'flex', flexDirection: 'row', margin: '1rem', alignItems: 'flex-end' }}>
+                    <p>Nome: </p>
+                    <span>{produto[0].nome}</span>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'row', margin: '1rem', alignItems: 'flex-end' }}>
+                    <p>Marca: </p>
+                    <span>{produto[0].marca}</span>
+                </div>
+            </div>
 
             <Link href={"/listaprodutos"}>Home</Link>
         </>
