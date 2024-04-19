b1273707d0127db4119a4187da5145c95e2b1a40
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-04-19 11:25:45 -0300

    19-04

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
       <h1>Hello, Next.js!</h1>
       <h2>Título 2</h2>
       <h3>Título 3</h3> 
-      <Link href={"/sobre"}>Sobre</Link>
+      <p><Link href={"/listaprodutos"}>Lista de produtos</Link></p>
+      <p><Link href={"/sobre"}>Sobre</Link></p>
     </main>
   );
 }
